e32a34276bb758037626dd3c37016c3b94246378
tenghx <68494740+tenghx@users.noreply.github.com>
2023-11-14 15:15:06 +0800

    Fix space again

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ GOOGLE_CHINA_URL='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-li
 HEAD_FILTER_PATTERN='s#^server=/##g'
 TAIL_FILTER_PATTERN='s#/114.114.114.114$##g'
 
-UPSTREAM_DNS_PATTERN_1='s#^.*$#https://1.1.1.1/dns-query\nhttps://[2606:4700:4700::1001]/dns-query\n[/&]https://223.5.5.5/dns-query\shttps://[2400:3200:baba::1]/dns-query#g'
-UPSTREAM_DNS_PATTERN_2='s#^.*$#\n[/&]https://223.5.5.5/dns-query\shttps://[2400:3200:baba::1]/dns-query#g'
+UPSTREAM_DNS_PATTERN_1='s#^.*$#https://1.1.1.1/dns-query\nhttps://[2606:4700:4700::1001]/dns-query\n[/&]https://223.5.5.5/dns-query https://[2400:3200:baba::1]/dns-query#g'
+UPSTREAM_DNS_PATTERN_2='s#^.*$#\n[/&]https://223.5.5.5/dns-query https://[2400:3200:baba::1]/dns-query#g'
 
 UPSTREAM_DNS_SPECIAL='\n[/g.cn/gkecnapps.cn/google.cn/googleapis.cn/googlecnapps.cn/gstatic.cn/gstaticcnapps.cn/]#'
 
@@ -20,7 +20,7 @@ UPSTREAM_DNS_FILE='upstream_dns.conf'
 mkdir -p publish
 cd publish
 
-curl $ACCELERATED_CHINA_URL | sed '/#/d' | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | sed '#^$#d' | sort -u | tr "\n" "/" | sed -r $UPSTREAM_DNS_PATTERN_1 > $UPSTREAM_DNS_FILE
-curl $APPLE_CHINA_URL | sed '/#/d' | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | sed '#^$#d' | sort -u | tr "\n" "/" | sed -r $UPSTREAM_DNS_PATTERN_2 >> $UPSTREAM_DNS_FILE
-#curl $GOOGLE_CHINA_URL | sed '/#/d' | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | sed '#^$#d' | sort -u | tr "\n" "/" | sed -r $UPSTREAM_DNS_PATTERN_2 >> $UPSTREAM_DNS_FILE
+curl $ACCELERATED_CHINA_URL | sed '/#/d' | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | sed '#^$#d' | sort -u | tr "\n" "/" | sed -r "$UPSTREAM_DNS_PATTERN_1" > $UPSTREAM_DNS_FILE
+curl $APPLE_CHINA_URL | sed '/#/d' | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | sed '#^$#d' | sort -u | tr "\n" "/" | sed -r "$UPSTREAM_DNS_PATTERN_2" >> $UPSTREAM_DNS_FILE
+#curl $GOOGLE_CHINA_URL | sed '/#/d' | sed -r $HEAD_FILTER_PATTERN | sed -r $TAIL_FILTER_PATTERN | sed '#^$#d' | sort -u | tr "\n" "/" | sed -r "$UPSTREAM_DNS_PATTERN_2" >> $UPSTREAM_DNS_FILE
 printf $UPSTREAM_DNS_SPECIAL >> $UPSTREAM_DNS_FILE
