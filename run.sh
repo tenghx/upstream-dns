b771f5adcbbf6b4c50987daad91d431c8c93060d
tenghx <68494740+tenghx@users.noreply.github.com>
2024-11-25 13:27:11 +0800

    refine run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,17 @@ ACCELERATED_CHINA='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-l
 APPLE_CHINA='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf'
 #GOOGLE_CHINA='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf'
 
-PATTERN_1='s#^.*$#https://1.1.1.1/dns-query\nhttps://[2606:4700:4700::1001]/dns-query\n[/&]https://223.5.5.5/dns-query https://[2400:3200:baba::1]/dns-query#g'
-PATTERN_2='s#^.*$#\n[/&]https://223.5.5.5/dns-query https://[2400:3200:baba::1]/dns-query#g'
-#SPECIAL='\n[/g.cn/gkecnapps.cn/google.cn/googleapis.cn/googlecnapps.cn/gstatic.cn/gstaticcnapps.cn/]#'
+DEFAULT_DNS='https://1.1.1.1/dns-query\nhttps://[2606:4700:4700::1001]/dns-query'
+CHINA_DNS='s#^.*$#\n[/&]https://223.5.5.5/dns-query https://[2400:3200:baba::1]/dns-query#g'
+#EXCLUDED='\n[/g.cn/gkecnapps.cn/google.cn/googleapis.cn/googlecnapps.cn/gstatic.cn/gstaticcnapps.cn/]#'
 
 OUTFILE='upstream_dns.conf'
 
 mkdir -p publish
 cd publish
 
-curl $ACCELERATED_CHINA | awk -F/ 'BEGIN{ORS="/"} {print $2}' | sed -r "$PATTERN_1" > $OUTFILE
-curl $APPLE_CHINA | awk -F/ 'BEGIN{ORS="/"} {print $2}' | sed -r "$PATTERN_2" >> $OUTFILE
-#curl $GOOGLE_CHINA | awk -F/ 'BEGIN{ORS="/"} {print $2}' | sed -r "$PATTERN_2" >> $OUTFILE
-#printf $SPECIAL >> $OUTFILE
+printf $DEFAULT_DNS > $OUTFILE
+curl $ACCELERATED_CHINA | awk -F/ 'BEGIN{ORS="/"} {print $2}' | sed -r "$CHINA_DNS" >> $OUTFILE
+curl $APPLE_CHINA | awk -F/ 'BEGIN{ORS="/"} {print $2}' | sed -r "$CHINA_DNS" >> $OUTFILE
+#curl $GOOGLE_CHINA | awk -F/ 'BEGIN{ORS="/"} {print $2}' | sed -r "$CHINA_DNS" >> $OUTFILE
+#printf $EXCLUDED >> $OUTFILE
